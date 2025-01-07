c3c19ed069d678eb48c98a2b229ab13f11b0a6f0
Miguel Luis <miguelluisataideferreira68@gmail.com>
2025-01-06 21:18:26 -0300

    remove: unusual component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import FilterBar from "@/shared/components/filter-bar"
 import Footer from "@/shared/components/footer"
 import ImageGallery from "@/subdomains/Galery/components/image-gallery"
 
@@ -6,7 +5,9 @@ export default function Home() {
   return (
     <main className="flex flex-col min-h-screen">
       <section className="container mx-auto px-4 py-8 flex-grow">
-        <h1 className="text-4xl text-center font-bold mb-8">Galeria de Imagens</h1>
+        <h1 className="text-4xl text-center font-bold mb-8">
+          Galeria de Imagens
+        </h1>
         <ImageGallery />
       </section>
       <Footer />
